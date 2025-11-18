82c9b5908eef8cbe0705a5f0d58165b1c3cb08a6
AABAN49 <aabanislam811@gmail.com>
2025-11-18 14:20:40 +0600

    Add files via upload

---
+++ chatbot.js
@@ -0,0 +1,24 @@
+
+const jk = {
+    "hello": "hi",
+    "what is your favorite flower": "apple",
+    "what is your name": "I am a chatbot",
+    "what do you do every time": "My one and only work is to help the people",
+    "is you can help me": "Yes, of course",
+};
+
+function d(){
+    const b = document.querySelector(".b");
+    const gh = document.querySelector(".gh").value;
+    const gk = jk[gh];
+
+    b.innerHTML += `
+        <div class="msg">
+            ${gk ? gk : "I don't understand."}
+            <button class="k" onclick="yes(this)">DELETE</button>
+        </div>
+        `;
+}
+function yes(btn){
+    btn.parentElement.remove();
+}
